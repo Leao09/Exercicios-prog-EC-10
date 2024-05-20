c004a5f59f163fde4b8ccfcfec300510829628bb
felipe henrique moreira leao <felipe.leao@sou.inteli.edu.br>
2024-05-20 05:40:12 -0300

    url banco

--- ponderada-2/backend/fastApi/app/config.py
+++ ponderada-2/backend/fastApi/app/config.py
@@ -4,8 +4,8 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    # db_url: str = Field(..., env="DATABASE_URL")
-    db_url: str = "postgres://leao09:6FF0Pb6lV3eS2eTpMii4Gblu1T3wgKhL@dpg-cp5gj7v79t8c73ettnh0-a/data_67ec"
+    db_url: str = Field(..., env="DATABASE_URL")
+    
 
 
 settings = Settings()
