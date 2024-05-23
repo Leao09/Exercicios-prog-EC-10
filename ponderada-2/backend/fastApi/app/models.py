8d91b13347dd0fd7b3adcc65337e3198c9524648
felipe henrique moreira leao <felipe.leao@sou.inteli.edu.br>
2024-05-23 00:29:16 -0300

    att

--- ponderada-2/backend/fastApi/app/models.py
+++ ponderada-2/backend/fastApi/app/models.py
@@ -1,28 +1,32 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field
+
 
 class TaskSchema(BaseModel):
-    id : int = Field(default=None, gt=0)
+    id: int = Field(default=None)
     Name: str = Field(default=None)
     isDone: bool = Field(default=None)
     # Configuração criada para documentação do modelo
+
     class Config:
         schema_extra = {
-            "post_teste" : {
+            "post_teste": {
                 "Name": "ir na padaria",
                 "isDone": "false",
             }
         }
 
 # Classe para representar os usuários do sistema
+
+
 class UserSchema(BaseModel):
-    id : int = Field(default=None, gt=0)
-    Email : str = Field(default=None)
-    password : str = Field(default=None)
+    Email: str = Field(default=None)
+    password: str = Field(default=None)
+
     class Config:
         schema_extra = {
-            "schema_user" : {
+            "schema_user": {
                 "Email": "teste@mail.com",
-                "password":"123"
+                "password": "123"
             }
         }
 # Classe para o login dos usuários
